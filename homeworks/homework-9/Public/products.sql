616f60fbdeac6f81f41787c3b07ba8ced7172a4d
iharris5 <iharris5@fordham.edu>
2025-04-03 12:10:12 -0400

    Homework 9

--- homeworks/homework-9/Public/products.sql
+++ homeworks/homework-9/Public/products.sql
@@ -1,1 +1,16 @@
 USE `cookie eaters`;
+
+CREATE TABLE `merch`
+(
+	`product_id` int(11) NOT NULL AUTO_INCREMENT,
+	`name` varchar(80) NOT NULL,
+	`price` int(11) NOT NULL,
+	primary key (`product_id`)
+);
+
+insert into merch (name, price)
+values ('Cookie Hat', 35);
+insert into merch (name, price)
+values ('Cookie Jacket', 45);
+insert into merch (name, price)
+values ('Cookie Fanatic Hoodie', 60);
